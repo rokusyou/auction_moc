873bb0e38fb0f0789b37c235994f8bd537d25d53
rokusyou <uekiwork@gmail.com>
2020-09-20 16:47:58 +0900

    fix auction index page

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,4 +1,12 @@
 class AuctionsController < ApplicationController
   def index
+    @item = Item.first
+  end
+
+  def bid
+    @item = Item.find_by(id: params[:id])
+    @item.price = @item.price + 1000
+    @item.save
+    redirect_to  action: :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'auctions/index'
   resources :items
-  root to: 'items#index'
+  root to: 'auctions#index'
   post 'items/bid'
+  post 'auctions/bid'
 end
